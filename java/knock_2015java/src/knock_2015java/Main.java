67aa959dc60d221bed05c940afd99225a5a8c1b9
ryosuke <tmcit.miyazaki@gmail.com>
2015-04-11 00:56:45 +0900

    add knock002

--- java/knock_2015java/src/knock_2015java/Main.java
+++ java/knock_2015java/src/knock_2015java/Main.java
@@ -5,8 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		Knock k000 = new Knock000();
 		Knock k001 = new Knock001();
+		Knock k002 = new Knock002();
+		
+		
 		//k000.start();
-		k001.start();
+		//k001.start();
+		k002.start();
 	}
 
 }
