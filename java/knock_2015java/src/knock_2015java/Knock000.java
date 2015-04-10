9b5d27681dae6d828e9c1ca30967e7474faa2c83
ryosuke <tmcit.miyazaki@gmail.com>
2015-04-10 17:36:26 +0900

    first commit

---
+++ java/knock_2015java/src/knock_2015java/Knock000.java
@@ -0,0 +1,14 @@
+package knock_2015java;
+
+public class Knock000 extends Knock{
+	String tok;
+	StringBuffer sb;
+	public Knock000(){
+		tok = "stressed";
+		sb = new StringBuffer(tok);
+	}
+	
+	public void start(){
+		System.out.println(sb.reverse().toString());
+	}
+}
