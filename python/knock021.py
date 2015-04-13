f163ede764a85334037c25e8ed23555422f47c54
ryosuke <tmcit.miyazaki@gmail.com>
2015-04-13 20:04:29 +0900

    use regular expression

--- python/knock021.py
+++ python/knock021.py
@@ -3,7 +3,9 @@ python knock021.py ../Data/knock020/result
 '''
 
 import sys
+import re
 
+category = re.compile('\[\[Category\:.*\]\]')
 for line in open(sys.argv[1]):
-    if 'Category' in line:
+    if category.search(line) is not None:
         print line.strip()
