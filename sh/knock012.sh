07401744bb47cd7213baa856bb3eb680bb518a10
ryosuke <tmcit.miyazaki@gmail.com>
2015-04-13 14:55:04 +0900

    ouput to col1 and col2

--- sh/knock012.sh
+++ sh/knock012.sh
@@ -1,2 +1,4 @@
-# bash knock012.sh ../Data/hightemp.txt
-cut -f 1 -d $'\t' $1
+# bash knock012.sh ../Data/hightemp.txt ../Data/col1.txt ../Data/col2.txt
+cut -f 1 -d $'\t' $1 > $2
+cut -f 2 -d $'\t' $1 > $3
+
