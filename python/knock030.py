a6c4486b00ad4e701ca1c70aff9455f81ec60f4c
ryosuke <tmcit.miyazaki@gmail.com>
2015-04-15 12:47:45 +0900

    fix format unidic -> ipadic

--- python/knock030.py
+++ python/knock030.py
@@ -9,31 +9,24 @@ import pickle
 doc = list()
 sent = list()
 for line in open(sys.argv[1]):
-    spl = line.strip().split('\t')
-
+    spl_tmp = line.strip().split('\t')
     # EOSがきたら一文の終わり
-    if spl[0] == 'EOS':
+    if spl_tmp[0] == 'EOS':
         if len(sent) == 0:
             continue
         doc.append(sent)
         sent = list()
         continue
-
-    # なんもない行だったらもともと空行
-    if spl[0] == '':
-        continue
     
-    # どちらでもなければ形態素情報あり
+    spl = spl_tmp[1].split(',')
+    spl.insert(0, spl_tmp[0])
+
     morph = dict()
     morph['surface'] = spl[0]
     morph['base'] = spl[3]
-    morph['pos'] = spl[4]
+    morph['pos'] = spl[1]
+    morph['pos1'] = spl[7]
     
-    # 品詞細分類はあるやつとないやつがある
-    if len(spl) < 6:
-        morph['pos1'] = ''
-    else:
-        morph['pos1'] = spl[5]
     sent.append(morph)
 
 # オブジェクトをそのまま（シリアライズして）出力
