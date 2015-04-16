008f226fe0867fda9401e3201d7af40cae76438c
梶原智之 <moguranosenshi@mogura-no-MacBookPro.local>
2015-04-17 00:49:36 +0900

    02.py minor change

--- kajiwara/kajiwara_02.py
+++ kajiwara/kajiwara_02.py
@@ -2,7 +2,7 @@
 
 
 def main(word1, word2):
-    print "".join([char1+char2 for char1, char2 in zip(list(word1), list(word2))]).encode("utf-8")
+    print "".join([char1 + char2 for char1, char2 in zip(word1, word2)]).encode("utf-8")
 
 
 if __name__ == '__main__':
