45cbb91eb74007e5b5152818f7a286fd8edd0b1e
梶原智之 <moguranosenshi@mogura-no-MacBookPro.local>
2015-04-17 00:33:40 +0900

    01.py minor change

--- kajiwara/kajiwara_01.py
+++ kajiwara/kajiwara_01.py
@@ -1,8 +1,8 @@
 # coding:utf-8
 
 
-def main(sentence):
-    print "".join(list(sentence)[::2]).encode("utf-8")
+def main(word):
+    print "".join(list(word)[::2]).encode("utf-8")
 
 
 if __name__ == '__main__':
