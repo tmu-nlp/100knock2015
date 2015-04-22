b8df3effeb72b4a587c3d0e5d4e27e991b8a3e20
佐藤貴之 <satotakayuki@satoutakayuki-no-MacBook-Air.local>
2015-04-22 17:07:42 +0900

    first commit

---
+++ 05knock.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# _*_ coding: utf-8 _*_
+
+def n_gram(sentence, n):
+
+    e_w_list = sentence.split(" ")
+    
+    word_n_gram = []
+
+    for index in range(len(e_w_list)-n+1):
+        my_list = []
+	for i in range(index, index+n):
+            my_list.append(e_w_list[i])
+        word_n_gram.append(tuple(my_list))
+
+    print word_n_gram
+
+#    sentence_a = sentence.strip(" ")
+#    print sentence_a
+#    文字列中のスペースが削除されない
+
+    char_n_gram = []
+
+    e_c_list = sentence.split(" ")
+    e_c_c = "".join(e_c_list)
+    e_c_list = list(e_c_c)
+
+    for index in range(len(e_c_list)-n+1):
+        my_list = []
+        for i in range(index, index+n):
+            my_list.append(e_c_list[i])
+        char_n_gram.append(tuple(my_list))
+
+    print char_n_gram
+
+n_gram("I am an NLPer", 2)
