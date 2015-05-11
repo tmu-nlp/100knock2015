8d1ac0494bf745d46eeb9b99b54e4e7e7486cc94
ryosuke <tmcit.miyazaki@gmail.com>
2015-05-12 02:56:06 +0900

    first commit

---
+++ java/knock_2015java/src/knock_2015java/Knock012.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package knock_2015java;
+
+/**
+ * @author ryosuke
+ *
+ */
+public class Knock012 extends Knock {
+
+	/**
+	 * 
+	 */
+	public Knock012() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	/* (非 Javadoc)
+	 * @see knock_2015java.Knock#start()
+	 */
+	@Override
+	public void start() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
