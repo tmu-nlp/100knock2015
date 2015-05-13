11be3ba19ead235e763159a82304f59e6181ef6a
ryosuke <tmcit.miyazaki@gmail.com>
2015-05-13 15:09:22 +0900

    add new instance

--- java/knock_2015java/src/knock_2015java/Main.java
+++ java/knock_2015java/src/knock_2015java/Main.java
@@ -18,8 +18,16 @@ public class Main {
 		knock_list.add(new Knock009());
 		knock_list.add(new Knock010());
 		knock_list.add(new Knock011());
+		knock_list.add(new Knock012());
+		knock_list.add(new Knock013());
+		knock_list.add(new Knock014(5));
+		knock_list.add(new Knock015(5));
+		knock_list.add(new Knock016(6));
+		knock_list.add(new Knock017());
+		knock_list.add(new Knock018());
+		knock_list.add(new Knock019());
 		
-		knock_list.get(11).start();
+		knock_list.get(16).start();
 	}
 
 }
