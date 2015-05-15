036fca826cd684df1d8814b5913049ea02f83c01
ryosuke <tmcit.miyazaki@gmail.com>
2015-05-15 17:28:30 +0900

    [modify] rm if name ==

--- python/knock020.py
+++ python/knock020.py
@@ -6,9 +6,8 @@ python knock020.py ../Data/jawiki-country.json
 import json
 import sys
 
-if __name__ == '__main__':
-    for line in open(sys.argv[1]):
-        jdata = json.loads(line)
-        if jdata['title'] == u'イギリス':
-            print jdata['text']
-            break
+for line in open(sys.argv[1]):
+    jdata = json.loads(line)
+    if jdata['title'] == u'イギリス':
+        print jdata['text']
+        break
