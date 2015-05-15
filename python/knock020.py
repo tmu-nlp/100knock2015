95c967f5c9218c304d04c3dfcd287d244e85c11a
ryosuke <tmcit.miyazaki@gmail.com>
2015-05-15 17:27:36 +0900

    [modify] minor modify

--- python/knock020.py
+++ python/knock020.py
@@ -1,12 +1,14 @@
-#coding:utf-8
+# coding:utf-8
 '''
-python knock020.py ../Data/hightemp.txt
+python knock020.py ../Data/jawiki-country.json
 '''
 
 import json
 import sys
 
-for line in open(sys.argv[1]):
-    jdata = json.loads(line)
-    if jdata['title'] == u'イギリス':
-        print jdata['text'].encode('utf-8')
+if __name__ == '__main__':
+    for line in open(sys.argv[1]):
+        jdata = json.loads(line)
+        if jdata['title'] == u'イギリス':
+            print jdata['text']
+            break
