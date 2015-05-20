69836c2b090892d48e563067d85ba646076e01fb
ryosuke <tmcit.miyazaki@gmail.com>
2015-05-21 01:13:34 +0900

    [modify] use python extarnal reguler expression

--- python/knock022.py
+++ python/knock022.py
@@ -9,4 +9,4 @@ category = re.compile('\[\[Category\:(((?P<cat1>.*)\|+.*)|(?P<cat2>.*))\]\]')
 for line in open(sys.argv[1]):
     result = category.search(line)
     if result is not None:
-        print  result.group('cat1') if result.group('cat2') is None else result.group(4)
+        print  result.group('cat1') if result.group('cat2') is None else result.group('cat2')
