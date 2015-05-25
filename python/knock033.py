7af8ac7004322f1dd953fddf04d97500d083edf3
ryosuke <tmcit.miyazaki@gmail.com>
2015-05-25 18:41:05 +0900

    [add] first commit

---
+++ python/knock033.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+'''
+python knock032.py ../Data/neko.txt.mecab
+'''
+
+import sys
+import knock030 as marujirou
+
+doc = marujirou.get_morphs(sys.argv[1])
+
+for sent in doc:
+    for m in sent:
+        if m['pos'] == '名詞' and m['pos1'] == 'サ変接続':
+            print m['surface'], m['base']
