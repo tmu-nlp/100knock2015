cdb28fc45fe2ac524ee8b894c391e277c6a9b001
ryosuke <tmcit.miyazaki@gmail.com>
2015-05-25 11:34:38 +0900

    [modfy] functionalize

--- python/knock030.py
+++ python/knock030.py
@@ -1,37 +1,44 @@
 # coding:utf-8
-'''
-python python/knock030.py ../Data/neko.txt.mecab [out.pkl]
-'''
+"""python python/knock030.py ../Data/neko.txt.mecab [out.pkl]."""
 
 import sys
-import pickle
 
-doc = list()
-sent = list()
-for line in open(sys.argv[1]):
-    spl_tmp = line.strip().split('\t')
-    # EOSがきたら一文の終わり
-    if spl_tmp[0] == 'EOS':
-        if len(sent) == 0:
+
+def get_morphs(mecab_out):
+
+    doc = list()
+    sent = list()
+    for line in open(mecab_out):
+        spl_tmp = line.strip().split('\t')
+        # EOSがきたら一文の終わり
+        if spl_tmp[0] == 'EOS':
+            if len(sent) == 0:
+                continue
+            doc.append(sent)
+            sent = list()
             continue
-        doc.append(sent)
-        sent = list()
-        continue
+        
+        spl = spl_tmp[1].split(',')
+        spl.insert(0, spl_tmp[0])
     
-    spl = spl_tmp[1].split(',')
-    spl.insert(0, spl_tmp[0])
+        morph = dict()
+        morph['surface'] = spl[0]
+        morph['base'] = spl[3]
+        morph['pos'] = spl[1]
+        morph['pos1'] = spl[7]
+        
+        sent.append(morph)
 
-    morph = dict()
-    morph['surface'] = spl[0]
-    morph['base'] = spl[3]
-    morph['pos'] = spl[1]
-    morph['pos1'] = spl[7]
-    
-    sent.append(morph)
+    return doc
 
-# オブジェクトをそのまま（シリアライズして）出力
-pickle.dump(doc, open(sys.argv[2], 'w'))
-#for sent in doc:
-#    for m in sent:
-#        print '%s, %s, %s, %s' % (m['surface'], m['base'], m['pos'], m['pos1'])
 
+def main():
+    doc = get_morphs(sys.argv[1])
+
+    for sent in doc:
+        for m in sent:
+            print '%s, %s, %s, %s' % (m['surface'], m['base'], m['pos'], m['pos1'])
+
+
+if __name__ == '__main__':
+    main()
