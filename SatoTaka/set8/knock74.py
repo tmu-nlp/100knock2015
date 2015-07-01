1d1d01a5310803f9e771ef9b09e46669e76f0743
佐藤貴之 <satotakayuki@satoutakayuki-no-MacBook-Air.local>
2015-07-01 17:55:58 +0900

    modified

--- SatoTaka/set8/knock74.py
+++ SatoTaka/set8/knock74.py
@@ -12,9 +12,9 @@ def main():
     probablity  = float()
     
     for line in model_file:
-        if line.startswith("@") and "@" in line:
+        if line.startswith("@") and "bias" in line:
            bias = line.strip().split("\t")[1]
-        else:
+        elif "@" not in line:
            weight, word = line.strip().split("\t")
            word_weight[word] = float(weight)
 
@@ -24,15 +24,19 @@ def main():
         for word in words:
             if word in word_weight:
                score += word_weight[word]
-        if score >= 0:
-           print "+1 "+ line.strip(),
-        else:
-           print "-1 "+ line.strip(),
-        exponent = math.exp(-(-score + int(bias)))
+#        if score >= 0:
+#           print "+1 "+ line.strip(),
+#        else:
+#           print "-1 "+ line.strip(),
+        exponent = math.exp(-(score + int(bias)))
         probablity = 1/(1+exponent)
-        print probablity
+#        print probablity
+        if probablity >= 0.5:
+           print "+1 "+line.strip()+ str(probablity)
+        else:
+           print "-1 "+line.strip()+ str(probablity)
         exponent   = float()
-        probablity = float() 
+        probablity = float()
 
     model_file.close()
     input_file.close()
