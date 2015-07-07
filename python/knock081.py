e2e36dc4c0c3a431eaae142a7295861f6eebb0fd
ryosuke <tmcit.miyazaki@gmail.com>
2015-07-08 06:51:23 +0900

    [modify] minor modified

--- python/knock081.py
+++ python/knock081.py
@@ -6,8 +6,12 @@ python knock081.py ../Data/result/knock080/clean.txt ../Data/country_name_list.t
 import sys
 import re
 
+contries = list()
+for contry in open(sys.argv[2]):
+    contries.append(contry.strip())
+
 for line in open(sys.argv[1]):
-    for contry in open(sys.argv[2]):
-        pattern  = re.compile(contry.strip())
-        line = pattern.sub(lambda m: m.group().replace(' ', '_'), line)
+    for contry in contries:
+        if contry in line:
+            line = re.sub(contry, lambda m: m.group().replace(' ', '_'), line)
     print line
