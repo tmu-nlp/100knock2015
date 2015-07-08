0d642db8914487e2d746f4a824200ade48edbe62
ryosuke <tmcit.miyazaki@gmail.com>
2015-07-08 11:27:28 +0900

    [fix] add context count

--- python/knock083.py
+++ python/knock083.py
@@ -8,6 +8,7 @@ import pickle
 
 uni = dict()
 co_occ = dict()
+con = dict()
 
 for line in open(sys.argv[1]):
     spl = line.strip().split('\t')
@@ -15,13 +16,20 @@ for line in open(sys.argv[1]):
     for co in spl[1:]:
         key = "%s %s" % (spl[0], co)
         co_occ[key] = co_occ.get(key, 0) + 1
+        con[co] = con.get(co, 0) + 1
 N = len(co_occ.keys())
 
 for w, count in uni.items():
     print w, count 
+for co, count in con.items():
+    print co, count
+
 for co, count in co_occ.items():
     print co, count
+
+
 print "N:", N
 
-pickle.dump([uni, co_occ, N], open(sys.argv[2], 'w'))
+
+pickle.dump([uni, con, co_occ, N], open(sys.argv[2], 'w'))
 
