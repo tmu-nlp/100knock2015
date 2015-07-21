a5102954b35d3a415464e45f5b31b736ee040cb3
ryosuke <tmcit.miyazaki@gmail.com>
2015-07-22 05:05:23 +0900

    [fix] fix some

--- python/knock086.py
+++ python/knock086.py
@@ -1,14 +1,14 @@
 # coding:utf-8
 """
-python knock086.py ../Data/result/knock085/python/Xpca.pkl ../Data/result/knock083/python/result.pkl
+python knock086.py ../Data/result/knock085/python/Xpca.pkl ../Data/result/knock084/python/word2id.pkl
 """
 
 import sys
 import pickle
 
 Xpca = pickle.load(open(sys.argv[1]))
-uni,_,_,_ = pickle.load(open(sys.argv[2]))
+word2id = pickle.load(open(sys.argv[2]))
 
-i = uni.keys().index('United_States')
-print X[i,]
+i = word2id['United_States']
+print Xpca[i]
 
