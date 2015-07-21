872d755d0f3c7bb6734a6f927044ccbb1d80e145
ryosuke <tmcit.miyazaki@gmail.com>
2015-07-22 05:00:21 +0900

    [fix] added must word list

--- python/knock084.py
+++ python/knock084.py
@@ -9,16 +9,25 @@ import scipy.sparse as sp
 import math
 
 uni, con, co_occ, N = pickle.load(open(sys.argv[1]))
-Vt = len(uni.keys())
-Vc = len(con.keys())
+#Vt = len(uni.keys())
+#Vc = len(con.keys())
+Vt = 10000
+Vc = 10000
 X = sp.lil_matrix((Vt, Vc))
-for i, tok in enumerate(uni.keys()):
+word2id = dict()
+
+must = ["United_States", "U.S", "England", "Spain", "Madrid", "Athens"]
+for i, tok in enumerate(must + uni.keys()):
+    word2id[tok] = i
+    if i == Vt:
+        break
     for j, co in enumerate(con.keys()):
+        if j == Vc:
+            break
         if co_occ.get('%s %s' % (tok, co), 0) >= 10:
             # ppmi
             X[i, j] = max(0, math.log((1.0*N*co_occ['%s %s' % (tok, co)])/(uni[tok]*con[co]), 2))
-        else:
-            X[i, j] = 0
+
 print X
 pickle.dump(X, open(sys.argv[2], 'w'))
 
