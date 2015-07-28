095262fbf689619b2777aaef93a3ca6002e7e8f7
ryosuke <tmcit.miyazaki@gmail.com>
2015-07-29 04:26:43 +0900

    [add]first commit

---
+++ python/knock094.py
@@ -0,0 +1,17 @@
+# coding:utf-8
+"""
+python knock094.py ../Data/wordsim353/combined.csv ../Data/result/knock090/python/vectors.bin
+"""
+
+import sys
+from gensim.models import word2vec
+
+model = word2vec.Word2Vec.load_word2vec_format(sys.argv[2], binary=True)
+
+for line in open(sys.argv[1]):
+    words = line.strip().split(',')
+    if not model.__contains__(words[0]) or not model.__contains__(words[1]):
+        continue
+    sim = model.n_similarity(words[0], words[1])
+    print line.strip()+','+str(sim)
+
