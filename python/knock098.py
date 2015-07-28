f60c5ba0b7f40dad4cae93c9b1b008860385216a
ryosuke <tmcit.miyazaki@gmail.com>
2015-07-29 07:01:39 +0900

    [add]first commit

---
+++ python/knock098.py
@@ -0,0 +1,21 @@
+# coding:utf-8
+"""
+python knock098.py ../Data/result/knock096/python/country.pkl
+"""
+
+import sys
+import pickle
+import numpy as np
+import itertools
+from scipy.cluster.hierarchy import linkage, dendrogram
+from scipy.spatial.distance import pdist
+import matplotlib.pyplot as plt
+
+dic = pickle.load(open(sys.argv[1]))
+
+features = np.array(dic.values())
+
+model = linkage(pdist(features))
+dendrogram(model, labels=dic.keys(), orientation='right')
+plt.show()
+
