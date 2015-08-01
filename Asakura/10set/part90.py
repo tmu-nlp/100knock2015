4330fee2d51f4ec59f72531f58c0edafe3b73974
asakurayasunobu <tennisabc562@gmail.com>
2015-08-01 19:07:02 +0900

    econd

--- Asakura/10set/part90.py
+++ Asakura/10set/part90.py
@@ -5,9 +5,9 @@
 from gensim.models import word2vec
 import pickle
 
-data = word2vec.Text8Corpus('corpus.txt')
+data = word2vec.Text8Corpus('part81.txt')
 model = word2vec.Word2Vec(data, size=200)
 
 
 if __name__ == "__main__":
-    model.save('matrix_owadasan.model')
+    model.save('matrix.model')
